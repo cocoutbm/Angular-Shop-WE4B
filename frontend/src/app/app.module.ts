795db22229556f1049013c516f76eb7337281e45
simon <simon.illouz--laurent@utbm.fr>
2023-05-27 14:58:18 +0200

    ajout du module routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  // { path: '', component: HomeComponent },
+  // Autres routes
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
