b73041e27a48362e488f3c870f5134ede75610d8
simon <simon.illouz--laurent@utbm.fr>
2023-05-29 12:32:11 +0200

    Ajout arboresance du projet de base (juste les dossier et fichier sans leur code

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './components/product-list/product-list/product-list.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 import { CartComponent } from './components/cart/cart.component';
 
 const routes: Routes = [
